c6799bdfe8a881892f2b794eb87366b9b333b6b6
ckdev88 <ckdev88@outlook.com>
2025-02-12 00:30:28 -0300

    feat: anticipate screen change (moving to next excersize)

--- consolescript.js
+++ consolescript.js
@@ -1,12 +1,18 @@
 // TODO add detection, only activate if useful
 // TODO add Check detection and click
 // TODO add Backspace detection and click
+// TODO add some timeout or debounce per key press
 
 /** Array of letters in the page */
-const letters = []
+let letters = []
 
 /** Stack to enable backspace/undo */
-const lettersRemoved = []
+let lettersRemoved = []
+let tokens = document.getElementsByClassName('token')
+let currentQuizId
+if (document.querySelector('.quiz-instruction-wrapper')) {
+	currentQuizId = document.querySelector('.quiz-instruction-wrapper').id
+}
 
 /** Special characters to simplify */
 const charmap = {
@@ -39,8 +45,7 @@ function simplifyToken(token) {
  * Find letters, their id's and match it with charmap
  * @returns {void}
  */
-function composeLetters() {
-	const tokens = document.getElementsByClassName('token')
+function composeLetters(tokens) {
 	for (let token of tokens) {
 		const letterObj = {
 			id: token.getAttribute('id'),
@@ -50,22 +55,25 @@ function composeLetters() {
 	}
 }
 
-/** 
+/**
  * Main function where keys are pressed and buttons get pushed
  * @param letterKey {string}
  * @returns {void}
  */
 function checkKeyHit(letterKey) {
-	if (letters.length < 1) composeLetters()
+	if (letters.length < 1) composeLetters(tokens)
 	/** prevent javascript from getting ahead of itself */
 	let duplicate = false
 	if (letters.length > 0 && duplicate === false) {
 		for (let i = 0; i < letters.length; i++) {
 			duplicate = true
 			if (letters[i].letter === letterKey) {
-				document.getElementById(letters[i].id).click()
-				const remo = letters.splice(i,1)
-				lettersRemoved.push(remo[0])
+				const targetNode = document.getElementById(letters[i].id)
+				if (targetNode) {
+					targetNode.click()
+					const remo = letters.splice(i, 1)
+					lettersRemoved.push(remo[0])
+				}
 				break
 			}
 		}
@@ -76,7 +84,7 @@ function checkKeyHit(letterKey) {
 		} else if (letterKey === 'backspace') {
 			const button = document.querySelector('.token-deselect .btn')
 			if (button) button.click()
-			const redo = lettersRemoved.splice(-1,1)
+			const redo = lettersRemoved.splice(-1, 1)
 			if (lettersRemoved.length > 0) letters.push(redo[0])
 			duplicate = true
 		}
@@ -90,6 +98,12 @@ function checkKeyHit(letterKey) {
  */
 document.addEventListener('keyup', (event) => {
 	event.preventDefault()
+	if (document.querySelector('.quiz-instruction-wrapper').id !== currentQuizId) {
+		letters = []
+		lettersRemoved = []
+		tokens = document.getElementsByClassName('token')
+		currentQuizId = document.querySelector('.quiz-instruction-wrapper').id
+	}
 	let letterKey = event.key.toLowerCase()
 	checkKeyHit(letterKey)
 })
