1ad20d8123a27e01b96f50280a76e9d8a222dc1d
ckdev88 <ckdev88@outlook.com>
2025-02-17 20:02:51 -0300

    feat: add multiple choice and all associated function and conditions, refactor other answer types, cleanup

--- consolescript.js
+++ consolescript.js
@@ -7,9 +7,8 @@
 console.log('Loading mondlypress...')
 /** @type {string[]} */
 let letters = []
-/** @type {'letter'|'word'} - Types of answers that the mouse clicks */
+/** @type {'letter'|'word'|'multiplechoice'} - Types of answers that the mouse clicks */
 let answerType = 'letter'
-
 var wordCapturesLetters = ''
 
 /** @type {number} -- artificial delay to deal with heavy/slow scripting on Mondly-side */
@@ -75,10 +74,35 @@ function simplifyToken(token, charType = 'letter') {
 }
 
 /**
- * Find letters, their id's and match it with charmap
+ * @param {string} - word with possible gender prefix, example: (uma) casa
+ * @returns {string} - de-genderized word
+ */
+function removeGender(combi = '') {
+	if (combi[0] !== '(') {
+		return simplifyToken(combi, 'word')
+	}
+
+	/** @type {string[]} - array used to join and then return */
+	let rarr = combi.split('')
+	/** @type {string} - result to return, a string free of gender */
+	let r = ''
+	for (let i = 2; i < rarr.length; i++) {
+		// the gender is always in parentheses, so use this to detect closing
+		if (rarr[i] === ')') {
+			rarr = rarr.slice(i + 2, rarr.length)
+			break
+		}
+	}
+	// OPTIMIZE first joining the array, then in simplifyToken it gets spliced and joined again
+	r = rarr.join('')
+	r = simplifyToken(r, 'word')
+	return r
+}
+
 /**
  * Find letters, their id's in the DOM and match it with charmap
- * @param tokens {HTMLCollection}
+ * @param {HTMLCollection} tokens
+ * @param {'letter'|'word'} answerType - multiplechoice is handles separately in submitMultipleChoice
  * @returns {void}
  */
 function composeLetters(tokens) {
@@ -97,60 +121,133 @@ function composeLetters(tokens) {
 }
 
 /**
+ * Triggers de button push of the associated keypress letter
+ * @returns {void}
+ */
+function submitLetterOfLetters(letters, letterKey) {
+	setTimeout(() => {
+		if (letters.length > 0) {
+			for (let i = 0; i < letters.length; i++) {
+				if (letters[i].letter === letterKey) {
+					const targetNode = document.getElementById(letters[i].id)
+					if (targetNode) {
+						targetNode.click()
+						const remo = letters.splice(i, 1)
+						lettersRemoved.push(remo[0])
+					}
+					break
+				}
+			}
+		}
+	}, TIMEOUT_PRESS)
+}
+
+/**
+ * Triggers de button push of the word associated buffer `wordCapturesLetters` and manages this buffer
+ * @returns {void}
+ */
+function submitWordOfWords(letters) {
+	setTimeout(() => {
+		for (let i = 0; i < letters.length; i++) {
+			if (letters[i].letter === wordCapturesLetters) {
+				const targetNode = document.getElementById(letters[i].id)
+				if (targetNode) {
+					targetNode.click()
+					const remo = letters.splice(i, 1)
+					lettersRemoved.push(remo[0])
+				}
+				break
+			}
+		}
+		wordCapturesLetters = ''
+		typeShower(wordCapturesLetters)
+	}, TIMEOUT_PRESS)
+}
+
+function submitMultipleChoice() {
+	// trigger button connected to id in answers
+	let multiplechoiceAnswers = []
+	for (let i = 0; i < 4; i++) {
+		// TODO this assumes always 4 options, create some checks for breaks
+		multiplechoiceAnswers.push({
+			answer: removeGender(document.getElementById(`option-${i}`)?.innerText),
+		})
+	}
+
+	for (i = 0; i < multiplechoiceAnswers.length; i++) {
+		if (wordCapturesLetters.trim() === multiplechoiceAnswers[i].answer) {
+			button = document.getElementById(`option-${i}`)
+			if (button) button.click()
+			break
+		}
+	}
+	setTimeout(() => {
+		button = document.querySelector('.quiz-action .btn')
+		button.click()
+	}, TIMEOUT_PRESS)
+}
+
+/**
  * Main function where keys are pressed and buttons get pushed
  * @param letterKey {string}
  * @returns {void}
  */
 function checkKeyHit(letterKey) {
 	let useTokens = false
-	if (document.getElementsByClassName('token').length > 0) useTokens = true
+	if (
+		document.getElementsByClassName('token').length > 0 ||
+		document.getElementById('option-0')?.innerText.length > 0
+	) {
+		useTokens = true
+	}
 
 	if (letterKey === '3') {
 		playAudio()
 	}
 	if (letterKey === 'Enter') {
-		let button = document.querySelector('.quiz-action .btn')
+		/** @type {Element|null} */
 		typeShower('', true)
-		if (!button) {
-			button = document.querySelector('.general-action .btn')
+		let button = null
+		if (answerType === 'multiplechoice') submitMultipleChoice()
+		else {
+			wordCapturesLetters = ''
+			button = document.querySelector('.quiz-action .btn')
 		}
-		if (button) {
-			button.click()
-		}
+		if (!button) button = document.querySelector('.general-action .btn')
+		if (button) button.click()
 	}
 
-	// if using letters and words which usually need a finger and/or a mouse
+	// if using letters, words and multiplechoice which usually need a finger and/or a mouse
 	if (useTokens) {
 		let quizInstructionWrapper = document.querySelector('.quiz-instruction-wrapper')
 		if (quizInstructionWrapper) {
+			let answerType = getAnswerType(tokens)
+
 			// check if quiz has changed, if yes, letters need to be re-composed
 			if (quizInstructionWrapper.id !== currentQuizId) {
 				// TODO this can/must be optimized
 				letters = []
 				lettersRemoved = []
-				tokens = document.getElementsByClassName('token')
-				composeLetters(tokens)
 				currentQuizId = document.querySelector('.quiz-instruction-wrapper').id
-				if (document.querySelector('.tokens-list .token').innerText.length > 1) {
-					answerType = 'word'
-				} else {
-					answerType = 'letter'
+				if (answerType !== 'multiplechoice') {
+					tokens = document.getElementsByClassName('token', answerType)
+					composeLetters(tokens, answerType)
 				}
 			}
 		}
 
 		if (letterKey === '1') {
 			// rebuild letters array // TODO this should not be necessary in a perfect world, if not used anymore, remove it
-			answerType = 'letter'
 			tokens = document.getElementsByClassName('token')
-			composeLetters(tokens)
+			answerType = getAnswerType()
+			if (tokens.length > 0) composeLetters(tokens)
 		}
 		if (letterKey === '2') {
 			// TODO this should not be necessary in a perfect world, if not used anymore, remove it
 			tokens = document.getElementsByClassName('token')
 			composeLetters(tokens)
 		} else if (letterKey === 'Backspace') {
-			if (answerType === 'word') {
+			if (answerType === 'word' || answerType === 'multiplechoice') {
 				wordCapturesLetters = wordCapturesLetters.slice(0, wordCapturesLetters.length - 1)
 				typeShower(wordCapturesLetters)
 			} else {
@@ -168,44 +265,15 @@ function checkKeyHit(letterKey) {
 					typeShower(wordCapturesLetters)
 				}
 			}
-
 			if (answerType === 'word' && letterKey === ' ') {
-				console.log('letters/words', letters)
-				setTimeout(() => {
-					// if (letters.length > 0) {
-					for (let i = 0; i < letters.length; i++) {
-						if (letters[i].letter === wordCapturesLetters) {
-							const targetNode = document.getElementById(letters[i].id)
-							console.log('gogogo')
-							if (targetNode) {
-								targetNode.click()
-								const remo = letters.splice(i, 1)
-								lettersRemoved.push(remo[0])
-								// console.log('removed letters:', lettersRemoved.join('').toString())
-							}
-							break
-						}
-					}
-					wordCapturesLetters = ''
+				submitWordOfWords(letters)
+			} else if (answerType === 'multiplechoice') {
+				if (letterKey.length === 1 && letterKey !== '1' && letterKey !== '2' && letterKey !== '3') {
+					wordCapturesLetters += letterKey
 					typeShower(wordCapturesLetters)
-				}, TIMEOUT_PRESS)
+				}
 			} else if (answerType === 'letter') {
-				// TODO not yet optimal, keeps stuck on answerType=word sometimes
-				setTimeout(() => {
-					if (letters.length > 0) {
-						for (let i = 0; i < letters.length; i++) {
-							if (letters[i].letter === letterKey) {
-								const targetNode = document.getElementById(letters[i].id)
-								if (targetNode) {
-									targetNode.click()
-									const remo = letters.splice(i, 1)
-									lettersRemoved.push(remo[0])
-								}
-								break
-							}
-						}
-					}
-				}, TIMEOUT_PRESS)
+				submitLetterOfLetters(letters, letterKey)
 			}
 		}
 	} else {
@@ -270,6 +338,18 @@ function typeShower(chars = '', clear = false) {
 		document.querySelector('.ember-application').prepend(ts)
 		ts.innerText = chars
 	}
+}
+
+/**
+ * @returns {'letter'|'word'|'multiplechoice'}
+ */
+function getAnswerType() {
+	let totalchars = 0
+	for (let token of tokens) totalchars += token.innerText.length
+	if (totalchars === tokens.length) answerType = 'letter'
+	if (totalchars > tokens.length) answerType = 'word'
+	if (document.getElementById('option-0')) answerType = 'multiplechoice'
+	return answerType
 }
 
 /**
